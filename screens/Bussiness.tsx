f42d890e6787373a5db6f6e53230da68ffedd23b
Arquimedes <aguiland@assanet.com>
2022-10-21 18:36:01 -0400

    add footer

--- screens/Bussiness.tsx
+++ screens/Bussiness.tsx
@@ -1,19 +1,102 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-
+import { SearchBar, FAB, Icon } from "@rneui/themed";
+import { LinearGradient } from "expo-linear-gradient";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import ContactList from "../components/ContactList";
 //{ navigation }: RootTabScreenProps<'TabOne'>
+
+const bussinessList = [
+  {
+    id: 1,
+    name: "Telepizza",
+    isFriend: false,
+  },
+  {
+    id: 2,
+    name: "Frangelica",
+    isFriend: false,
+  },
+  {
+    id: 3,
+    name: "Repuestos Negrillo",
+    isFriend: true,
+  },
+  {
+    id: 4,
+    name: "Burger King",
+    isFriend: true,
+  },
+  {
+    id: 5,
+    name: "Dominos Pizzas",
+    isFriend: true,
+  },
+  {
+    id: 6,
+    name: "Pizzeria Alfredo",
+    isFriend: true,
+  },
+];
+
 export default function Bussiness() {
+  const [search, setSearch] = useState("");
+  const [contacts, setContacts] = useState(bussinessList);
+
+  const updateSearch = (search: string) => {
+    setSearch(search);
+    setContacts(bussinessList.filter((v) => v.name.includes(search)));
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+      <View style={styles.searchContainer}>
+        <SearchBar
+          placeholder="Buscas un negocio en concreto?"
+          onChangeText={updateSearch}
+          value={search}
+          containerStyle={{
+            backgroundColor: "transparent",
+            borderWidth: 1,
+            borderColor: "#eaeaea",
+            borderRadius: 20,
+          }}
+          inputContainerStyle={{
+            backgroundColor: "transparent",
+            borderColor: "#eaeaea",
+          }}
+          inputStyle={{ backgroundColor: "white" }}
+          lightTheme
+        />
+      </View>
+
+      {contacts && contacts.length > 0 ? (
+        <ContactList list={contacts} />
+      ) : (
+        <Text style={{ marginVertical: 20, textAlign: "center" }}>
+          No se encontraron negocios.
+        </Text>
+      )}
+      <View style={styles.footerContainer}>
+        <LinearGradient
+          colors={["#3272A5", "#3E8CBC", "#469DCB"]}
+          style={{
+            paddingVertical: 10,
+            borderRadius: 10,
+            paddingHorizontal: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 200,
+          }}
+          start={{ x: -1, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
+          <Icon name="pluscircleo" type="antdesign" color="white" />
+          <Icon name="creditcard" type="antdesign" color="white" />
+          <Icon name="shoppingcart" type="antdesign" color="white" />
+        </LinearGradient>
+      </View>
     </View>
   );
 }
@@ -21,16 +104,18 @@ export default function Bussiness() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  searchContainer: {
+    marginTop: 10,
+    marginBottom: 5,
+    height: 70,
+    paddingHorizontal: 20,
+    width: "100%",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  footerContainer: {
+    position: "absolute",
+    alignSelf: "center",
+    bottom: 2,
+    paddingHorizontal: 40,
   },
 });
